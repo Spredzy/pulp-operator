4e37006645d47260777ef7ff922a11543f1f4090
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-02-03 19:01:47 -0300

    Adding Github Actions
    [noissue]

---
+++ .ci/scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+#!/usr/bin/env bash
+
+echo "Build pulp/pulpcore images"
+cd $GITHUB_WORKSPACE/containers/
+cp $GITHUB_WORKSPACE/.ci/ansible/vars.yaml vars/vars.yaml
+pip install ansible
+ansible-playbook -v build.yaml
+cd $GITHUB_WORKSPACE
+
+echo "Test pulp/pulpcore images"
+sudo -E ./up.sh
+.ci/scripts/pulp-operator-check-and-wait.sh
+.ci/scripts/pulp_file-tests.sh
+
+podman images
+
+echo "Deploy pulp-operator"
+sudo -E PODMAN_OR_DOCKER=docker $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
+
+echo "Deploy pulp latest"
+sudo -E QUAY_REPO_NAME=pulp $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
+
+echo "Deploy pulpcore latest"
+sudo -E QUAY_REPO_NAME=pulpcore $GITHUB_WORKSPACE/.ci/scripts/quay-push.sh
